2c6998314db5e90ee53d16d26ffeae3d76385761
felixx675 <felixx.sts@gmail.com>
2026-04-20 13:42:38 +0800

    Added Holo Shield

---
+++ FelixxContentPackCode/Relics/HoloShield.cs
@@ -0,0 +1,76 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using FelixxContentPack.FelixxContentPackCode.Extensions;
+using Godot;
+
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace FelixxContentPack.FelixxContentPackCode.Relics;
+
+[Pool(typeof(SharedRelicPool))]
+public class HoloShield : CustomRelicModel
+{
+    public override string PackedIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".RelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "holoshield.png".RelicImagePath();
+        }
+    }
+
+    protected override string PackedIconOutlinePath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_outline.png".RelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "holoshield_outline.png".RelicImagePath();
+        }
+    }
+
+    protected override string BigIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigRelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "holoshield.png".BigRelicImagePath();
+        }
+    }
+
+    public override RelicRarity Rarity => RelicRarity.Common;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        new[]
+        {
+            new BlockVar(0M, ValueProp.Unpowered)
+        };
+
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext context, Player player)
+    {
+        if (player != Owner)
+            return;
+
+        int turn = Owner.Creature.CombatState.RoundNumber;
+
+        if (turn <= 0)
+            return;
+
+        Flash();
+
+        // IMPORTANT: reuse DynamicVar but overwrite value safely
+        var blockVar = new BlockVar((decimal)turn, ValueProp.Unpowered);
+
+        await CreatureCmd.GainBlock(
+            Owner.Creature,
+            blockVar,
+            null
+        );
+    }
+}
